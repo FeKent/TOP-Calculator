09ef3c410c8a3f8876bf9428aaf7e102cc22f58d
SnippyRex <kentf022@gmail.com>
2023-03-26 19:56:22 +0100

    Script: Add stand in variables and add basic math functions

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let firstNumber = 0
+let secondNumber = 0
+let operator = ' '
+
+function add(firstNumber, secondNumber){
+    result = firstNumber + secondNumber
+    return result
+}
+
+function subtract(firstNumber, secondNumber){
+    result = firstNumber - secondNumber
+    return result
+}
+
+function multiply(firstNumber, secondNumber){
+    result = firstNumber * secondNumber
+    return result
+}
+
+function divide(firstNumber, secondNumber){
+    result = firstNumber / secondNumber
+    return result
+}
