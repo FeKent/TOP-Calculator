dad297a564d32d67aea1b2153aa0a088be57eeed
SnippyRex <kentf022@gmail.com>
2023-03-28 22:41:41 +0100

    Fix: Change output from class to ID to avoid issues with textContent

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const calcInput = document.querySelector('.content')
 const allButtons = document.querySelectorAll('button')
-const output = document.getElementsByClassName('output')
+const output = document.getElementById('output')
 
 allButtons.forEach(button => {
     button.addEventListener('click', (e) => {
        let value = e.target.dataset.value
        let displayValue = value
-       output.innerHTML = displayValue
+       output.textContent = displayValue
        console.log(value)
     } )
 })
