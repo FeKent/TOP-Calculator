5e9a4a9f2ed2a6e6c6896b25799bb24129f58306
SnippyRex <kentf022@gmail.com>
2023-03-29 01:19:10 +0100

    Script: Enable output to store multiple numbers at once, then move that value to new variable once an operator is chosen

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 const calcInput = document.querySelector('.content')
-const allButtons = document.querySelectorAll('button')
+const numbers = document.querySelectorAll('.numbers')
+const operators = document.querySelectorAll('.operators')
 const output = document.getElementById('output')
+const clear = document.querySelectorAll('.clear')
 
-allButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-       let value = e.target.dataset.value
-       let displayValue = value
-       output.textContent = displayValue
-       console.log(value)
-    } )
-})
 
+
+// allButtons.forEach(button => {
+//     button.addEventListener('click', (e) => {
+//        let value = e.target.dataset.value
+//        let displayValue = value
+//        output.textContent = displayValue
+//        console.log(value)
+//     } )
+// })
+//* Redone so that multiple values will be displayed in the output bar
+
+
+let currentValue = ''
+let previousValue = ''
+let selectedOperator = ''
+
+numbers.forEach((number) => number.addEventListener('click', function(e){
+    handleNumber(e.target.textContent)
+    output.textContent = currentValue;
+}))
+
+operators.forEach((op) => op.addEventListener('click', function(e){
+    handleOperator(e.target.textContent)
+    output.textContent = operator
+}))
+
+
+function handleNumber (num){
+    if(currentValue.length <= 5){
+        currentValue += num;
+    }
+}
+
+function handleOperator (op){
+    if(op == "x"){
+        operator = '*'
+    } else {
+        operator = op
+    }
+    previousValue = currentValue;
+    currentValue = '';
+}
